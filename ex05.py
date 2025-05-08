918725bcbf4a6279cd5cf19b914cc2841a306c66
MiguelP <77172963+Miguelzin635@users.noreply.github.com>
2025-05-08 15:51:14 -0300

    Update ex05.py

--- ex05.py
+++ ex05.py
@@ -1,6 +1,6 @@
 nomes = []
 bonus = []
-for i in range(3):
+for i in range(15):
     nome = str(input('\nInforme o nome: '))
     compra = float(input('Informe o valor de suas compras no ano passado: '))
     if compra < 1000:
@@ -14,4 +14,4 @@ for i in range(len(nomes)):
     print(f'O cliente {nomes[i]} ganhou um bônus de {bonus[i]:.2f}')
 
 
-input('Pressione Enter para fechar o programa...')
+input('\nPressione Enter para fechar o programa...')
