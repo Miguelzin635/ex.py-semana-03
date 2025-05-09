52d0e96540563ce3f418e9d8948cd08b8b8f0d39
MiguelP <77172963+Miguelzin635@users.noreply.github.com>
2025-05-09 13:31:44 -0300

    Update ex08.py

--- ex08.py
+++ ex08.py
@@ -2,8 +2,8 @@ i = 1
 vista = 0
 prazo = 0
 
-while i <= 15:
-    codigo = input('Digite o código da transação("V" para à vista e "P" a prazo )')
+while i <= 3:
+    codigo = input('Digite o código da transação("V" para à vista e "P" a prazo ): ')
     valor = float(input('Digite o valor da transação: '))
 
     if codigo == "v" or codigo == "V":
@@ -11,4 +11,11 @@ while i <= 15:
     elif codigo == "p" or codigo == "P":
         prazo = prazo + valor
     else: print('Código inválido!')
-i = i + 1
+    i = i + 1
+
+print(f'\nO valor total das compras à vista é: {vista:.2f}')
+print(f'O valor total das compras a prazo é: {prazo:.2f}')
+print(f'O valor t otal das compras efetuadas é: {vista + prazo:.2f}')
+print(f'O valor da primeira prestação, sendo que essas serão pagas em três vezes: {prazo / 3:.2f}')
+
+input('Pressione Enter para sair do programa...')
